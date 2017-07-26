f479eaaa641851ce5aad71362c99cc14fea83de1
liuru@163.com <1457816105@qq.com>
2017-07-26 15:48:40 +0800

    second push

---
+++ out/artifacts/webStore_war_exploded/WEB-INF/classes/Resource/shoping.sql
@@ -0,0 +1,26 @@
+create table items(
+id int(11) NOT NULL AUTO_INCREMENT,
+name varchar(20) ,
+city varchar(20),
+price decimal(10,2),
+num   int,
+pic   varchar(20),
+primary key(id))ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=utf8;
+
+insert into items(name,city,price,num,pic) value("威尔胜","北京",139.00,200,"./images/1.jpg");
+insert into items(name,city,price,num,pic) value("威尔胜","北京",139.00,200,"./images/2.jpg");
+insert into items(name,city,price,num,pic) value("斯伯丁","北京",149.00,200,"./images/3.jpg");
+insert into items(name,city,price,num,pic) value("联想","北京",5456.00,200,"./images/5.jpg");
+insert into items(name,city,price,num,pic) value("苹果","北京",15456.00,200,"./images/6.jpg");
+insert into items(name,city,price,num,pic) value("ThinkPad","北京",6456.00,200,"./images/7.jpg");
+insert into items(name,city,price,num,pic) value("华硕","北京",4656.00,200,"./images/8.jpg");
+insert into items(name,city,price,num,pic) value("帽子","北京",56.00,200,"./images/9.jpg");
+insert into items(name,city,price,num,pic) value("帽子","北京",67.00,200,"./images/11.jpg");
+insert into items(name,city,price,num,pic) value("T恤","北京",157.00,200,"./images/12.jpg");
+insert into items(name,city,price,num,pic) value("T恤双人件","北京",157.00,200,"./images/13.jpg");
+insert into items(name,city,price,num,pic) value("卡通T恤","北京",157.00,200,"./images/14.jpg");
+
+
+
+
+
